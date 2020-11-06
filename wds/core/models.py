2443466869467fc1d8f66c06f02e31f460fb4f99
Rahul Goyal <rahul@rahul.org>
2020-11-06 12:41:24 -0500

    Changed login page

--- wds/core/models.py
+++ wds/core/models.py
@@ -84,7 +84,7 @@ class trade(models.Model):
     seller=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stock=models.CharField(choices=stock_list,max_length=100)
     numberofstocks=models.IntegerField(default=0)
-    priceperstock=models.FloatField(null=True, blank=True)
+    priceperstock=models.FloatField(null=True, blank=True, default=0.0)
     buyer=models.ForeignKey(settings.AUTH_USER_MODEL,related_name='buyer_of_stock', on_delete=models.CASCADE)
     #userbalance=models.FloatField(default=1000000.0)
     
